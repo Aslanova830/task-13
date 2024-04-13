cfaccebfff5bdc527f14e8fee4e1fa02c66ff03d
LalaAslanova <lalAslanova830@gmail.com>
2024-04-14 00:43:08 +0400

    js

---
+++ js/javascript.js
@@ -0,0 +1,59 @@
+const number1 = 24;
+const number2 = 12;
+
+let str = "123";
+alert(typeof str);
+
+alert(number1-number2);
+alert(number1*number2);
+alert(number1/number2);
+
+if( number1%2==0){
+    alert(cut);
+}
+else{
+    alert(tek);
+}
+max(number1,number2);
+
+min(number1,number2);
+
+let sum = function(number1, number2) {
+    return number1+number2;
+  };
+
+a=(number1+number2)/2;
+
+
+let reversed = reverseString('hi');
+console.log(reversed);
+
+while(i<100){
+    a=i%10;
+    b=i/10;
+    if(b/a==1) {
+        alert(i);
+    }
+}
+
+function convertToUppercase(str) {
+    return str.toUpperCase();
+}
+
+let st = "hello world";
+console.log(convertToUppercase(st));
+
+function convertToLowercase(str) {
+    return str.toLowerCase();
+}
+let d = "HELLO WORLD";
+console.log(convertToLowercase(d));
+
+let A= "hello world! ";
+console.log(capitalizeFirstLetterOfEachWord(A));
+
+function generateRandomInteger(number1,number2)
+{
+   number2+1;
+   number1-1;
+}
